1132a7756aaeb37e6a017669cd95a987f43c60b8
Никита Гребнев <nikitagrebnev@MacBook-Air-Nikita.local>
2025-01-26 15:59:19 +0600

    docker added

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET')
 
 DEBUG = config('DEBUG')
 
@@ -68,6 +68,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT')
--- tests/test_patients/test_patients_api.py
+++ tests/test_patients/test_patients_api.py
@@ -2,3 +2,4 @@ from tests.test_core.test_base_class import BaseTest
 
 
 class PatientAPITest(BaseTest):
+    ...
